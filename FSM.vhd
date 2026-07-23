2c4715846b21198afb2c27f95fd7eb7a62ef6d82
Enzo Giacomini <enzo-giacomini@hotmail.com>
2026-07-23 17:11:57 -0300

    feat: otimização e adaptação para TEMA 4

--- FSM.vhd
+++ FSM.vhd
@@ -3,142 +3,186 @@ USE IEEE.std_logic_1164.all;
 
 Entity FSM is
 Port(DataIn : in std_logic_vector(7 downto 0);
-	  clk,reset : in std_logic;
-	  N,Z,cout,ov : out std_logic;
-	  DataOut,R0,R1,R2,R3,R4,R5,R6,R7 : out std_logic_vector(7 downto 0)
-	  );
+     clk,reset : in std_logic;
+     N,Z,cout,ov : out std_logic;
+     DataOut,R0,R1,R2,R4 : out std_logic_vector(7 downto 0)
+     );
 end FSM;
 
 architecture arq of FSM is
 
 component datapath is
 Port(DataIn : in std_logic_vector(7 downto 0);
-     C : in std_logic_vector(27 downto 0);
-	  clk,reset : in std_logic;
-	  N,Z,cout,ov : out std_logic;
-	  DataOut,R0,R1,R2,R3,R4,R5,R6,R7 : out std_logic_vector(7 downto 0)
-	  );
+     C : in std_logic_vector(34 downto 0);
+     clk,reset : in std_logic;
+     N,Z,cout,ov : out std_logic;
+     DataOut,R0,R1,R2,R4 : out std_logic_vector(7 downto 0)
+     );
 end component;
 
-type t_state is (Sreset,S0,S1,S2,S3,S4,S5);
-signal estado_atual, prox_estado : t_state; 
-signal C : std_logic_vector(27 downto 0);
+-- DataIn recebe, em ciclos consecutivos: 1, 10 e n.
+-- R0 = constante 1
+-- R1 = contador
+-- R2 = numero atual
+-- R4 = acumulador e DataOut
+-- Tem que guardar a soma no R4 porque ele e o registrador que vai para DataOut.
 
+--ESTADOS:
+type t_state is (
+    Sreset, --Reseta
+    S_carrega_um, --DataIn Constante 1
+    S_carrega_dez, --DataIn Constante 10 (controle de soma)
+    S_carrega_n, --DataIn o numero que inicia as somas sucessivas
+    S_copia_n, --R4 recebe o valor inicial de R2
+    --Inicio LOOP:
+    S_atualiza, --Incrementa R2 e decrementa o controlador no mesmo ciclo
+    S_acumula, --Acumula na soma e testa o fim do loop
+    --FIM LOOP
+    S_fim --DataOut
+);
+
+signal estado_atual, prox_estado : t_state;
+signal C : std_logic_vector(34 downto 0);
+signal N_dp, Z_dp, cout_dp, ov_dp : std_logic; --Pegar as flags do datapath
+
+-- SOBRE A PALAVRA CONTROLE:
+-- 0 a 7 = DataIn (0) ou Data_U (1)
+-- 8 a 15 = Load (1) ou Nao Load (0)
+-- 18 a 16 = Seleciona Registrador para opA1 (3 bits)
+-- 21 a 19 = Seleciona Registrador para opB1 (3 bits)
+-- 23 a 22 = Seleciona Registrador para DataOut ou Shifter (2 bits)
+-- 24 = SOMA ou SUB do primeiro somador
+-- 25 = ShiftL ou ShiftR
+-- 26 = Resultado SOMA/SUB ou SHIFTL/SHIFTR
+-- 27 = opA1 ou Resultado do SOMA/SUB/SHIFTL/SHIFTR
+-- 30 a 28 = Seleciona Registrador para opA2 (3 bits)
+-- 33 a 31 = Seleciona Registrador para opB2 (3 bits)
+-- 34 = SOMA ou SUB do segundo somador
 
 begin
 
 P1: process(clk,reset)
 begin
-   if reset = '1' then
-	   estado_atual <= Sreset;
-		
-	elsif clk'event and clk = '1' then
-	   estado_atual <= prox_estado;
-	end if;
+    if reset = '1' then
+        estado_atual <= Sreset;
+    elsif clk'event and clk = '1' then
+        estado_atual <= prox_estado;
+    end if;
 end process;
 
-P2: process(estado_atual,reset)
+P2: process(estado_atual,reset,Z_dp)
 begin
-  case estado_atual is
-      when Sreset => if reset = '1' then
-		                  prox_estado <= Sreset;
-							else
-							   prox_estado <= S0;
-							end if;
-		
-		when S0 => --R0=dataIn e R1=dataIn = 15
-		     C(7 downto 0)  <= "00000000";--Mux2:1 0-DataIn  1-Data_Ula
-			  C(15 downto 8) <= "00000011";--load
-			  C(18 downto 16) <= "000"; -- OpA
-			  C(21 downto 19) <= "001"; -- OpB
-			  C(23 downto 22) <= "00"; -- OpC
-			  C(24) <= '0'; -- 0-soma  1-subtrai
-			  C(25) <= '0'; -- 0- SLL  1-SRL
-			  C(26) <= '0'; -- 0- Soma/Sub  1-Shifter
-			  C(27) <= '0'; -- 0- DataUla  1-OpA
-			  prox_estado <= S1;
-			  
-		when S1 => --R4=R0+R1 R4=30
-		     C(7 downto 0)  <= "00010000";--Mux2:1 0-DataIn  1-Data_Ula
-			  C(15 downto 8) <= "00010000";--load
-			  C(18 downto 16) <= "000"; -- OpA=R0
-			  C(21 downto 19) <= "001"; -- OpB=R1
-			  C(23 downto 22) <= "00"; -- OpC/DataOut=R4
-			  C(24) <= '0'; -- 0-soma  1-subtrai
-			  C(25) <= '0'; -- 0- SLL  1-SRL
-			  C(26) <= '0'; -- 0- Soma/Sub  1-Shifter
-			  C(27) <= '0'; -- 0- DataUla  1-OpA
-			  prox_estado <= S2;
 
-		when S2 => --R5=SLL(R4) R5=60
-		     C(7 downto 0)  <= "00100000";--Mux2:1 0-DataIn  1-Data_Ula
-			  C(15 downto 8) <= "00100000";--load
-			  C(18 downto 16) <= "000"; -- OpA=R0
-			  C(21 downto 19) <= "000"; -- OpB=R0
-			  C(23 downto 22) <= "00"; -- OpC/DataOut=R4
-			  C(24) <= '0'; -- 0-soma  1-subtrai
-			  C(25) <= '0'; -- 0- SLL  1-SRL
-			  C(26) <= '1'; -- 0- Soma/Sub  1-Shifter
-			  C(27) <= '0'; -- 0- DataUla  1-OpA
-			  prox_estado <= S3;  
-		
-		when S3 => --R6=SRL(R4) R6=15
-		     C(7 downto 0)  <= "01000000";--Mux2:1 0-DataIn  1-Data_Ula
-			  C(15 downto 8) <= "01000000";--load
-			  C(18 downto 16) <= "000"; -- OpA=R0
-			  C(21 downto 19) <= "000"; -- OpB=R0
-			  C(23 downto 22) <= "00"; -- OpC/DataOut=R4
-			  C(24) <= '0'; -- 0-soma  1-subtrai
-			  C(25) <= '1'; -- 0- SLL  1-SRL
-			  C(26) <= '1'; -- 0- Soma/Sub  1-Shifter
-			  C(27) <= '0'; -- 0- DataUla  1-OpA
-			  prox_estado <= S4;
-			  
-		when S4 => 
-			  C(7 downto 0)  <= "01000000";--Mux2:1 0-DataIn  1-Data_Ula
-			  C(15 downto 8) <= "01000000";--load
-			  C(18 downto 16) <= "000"; -- OpA=R0
-			  C(21 downto 19) <= "000"; -- OpB=R0
-			  C(23 downto 22) <= "10"; -- OpC/DataOut=R6
-			  C(24) <= '0'; -- 0-soma  1-subtrai
-			  C(25) <= '1'; -- 0- SLL  1-SRL
-			  C(26) <= '1'; -- 0- Soma/Sub  1-Shifter
-			  C(27) <= '0'; -- 0- DataUla  1-OpA
-			  prox_estado <= S5;
-		
+    --Comeca o algoritmo com tudo zerado, logo inicialmente ja ta tudo setado para DataIn
+    C <= (others => '0');
+    prox_estado <= estado_atual;
 
-		when S5 => 
-			  C(7 downto 0)  <= "00000000";--Mux2:1 0-DataIn  1-Data_Ula
-			  C(15 downto 8) <= "00000000";--load
-			  C(18 downto 16) <= "000"; -- OpA=R0
-			  C(21 downto 19) <= "000"; -- OpB=R0
-			  C(23 downto 22) <= "00"; -- OpC/DataOut=R6
-			  C(24) <= '0'; -- 0-soma  1-subtrai
-			  C(25) <= '1'; -- 0- SLL  1-SRL
-			  C(26) <= '1'; -- 0- Soma/Sub  1-Shifter
-			  C(27) <= '1'; -- 0- DataUla  1-OpA
-			  prox_estado <= S0;
-		
-  end case;
-		
+    case estado_atual is
+        when Sreset =>
+            if reset = '0' then
+                prox_estado <= S_carrega_um;
+            end if;
+
+        when S_carrega_um =>
+            -- R0 <- DataIn (1)
+            -- O 1 fica guardado para incrementar R2 e decrementar R1.
+            C(15 downto 8) <= "00000001"; --Load R0
+            prox_estado <= S_carrega_dez;
+
+        when S_carrega_dez =>
+            -- R1 <- DataIn (10)
+            -- R1 controla quantas vezes o loop vai repetir.
+            C(15 downto 8) <= "00000010"; --Load R1
+            prox_estado <= S_carrega_n;
+
+        when S_carrega_n =>
+            -- R2 <- DataIn (n)
+            -- R2 guarda o numero que vai sendo incrementado.
+            C(15 downto 8) <= "00000100"; --Load R2
+            prox_estado <= S_copia_n;
+
+        when S_copia_n =>
+            -- R4 <- R2, sem operacao aritmetica.
+            -- R4 comeca com n porque o numero lido tambem entra na soma.
+            -- Tem que guardar no R4 porque ele e quem vai para DataOut.
+            C(7 downto 0)   <= "00010000"; --Data_U
+            C(15 downto 8)  <= "00010000"; --Load R4
+            C(18 downto 16) <= "010"; --opA1 = R2
+            C(27) <= '1';              --DataUla1 = opA1
+            prox_estado <= S_atualiza;
+
+        when S_atualiza =>
+            -- Duas operacoes no mesmo ciclo:
+            -- Somador 1: R2 <- R2 + R0
+            -- Somador 2: R1 <- R1 - R0
+            -- Os dois somadores fazem essas contas ao mesmo tempo.
+            C(7 downto 0)   <= "00000110"; --Data_U para R1 e R2
+            C(15 downto 8)  <= "00000110"; --Load R1 e R2
+
+            C(18 downto 16) <= "010"; --opA1 = R2
+            C(21 downto 19) <= "000"; --opB1 = R0
+            C(24) <= '0';              --soma 1
+            C(26) <= '0';              --resultado somador 1
+            C(27) <= '0';              --DataUla1
+
+            C(30 downto 28) <= "001"; --opA2 = R1
+            C(33 downto 31) <= "000"; --opB2 = R0
+            C(34) <= '1';              --subtracao 2
+
+            prox_estado <= S_acumula;
+
+        when S_acumula =>
+            -- Somador 1: R4 <- R4 + R2
+            -- Somador 2: testa R1 - R3, sendo R3 = 0
+            -- O zero e tirado do datapath pela posicao de R3.
+            -- R3 nao e um registrador fisico: essa posicao foi ligada em zero.
+            C(7 downto 0)   <= "00010000"; --Data_U
+            C(15 downto 8)  <= "00010000"; --Load R4
+            C(18 downto 16) <= "100"; --opA1 = R4
+            C(21 downto 19) <= "010"; --opB1 = R2
+            C(24) <= '0';
+            C(26) <= '0';
+            C(27) <= '0';
+
+            C(30 downto 28) <= "001"; --opA2 = R1
+            C(33 downto 31) <= "011"; --opB2 = R3 = 0
+            C(34) <= '1';              --subtracao 2
+
+            -- A soma atual acontece antes da saida do loop.
+            if Z_dp = '1' then
+                prox_estado <= S_fim;
+            else
+                prox_estado <= S_atualiza;
+            end if;
+
+        when S_fim =>
+            -- DataOut <- R4. Mantem o resultado ate reset.
+            -- O resultado fica parado na saida ate receber outro reset.
+            C(23 downto 22) <= "00";
+            prox_estado <= S_fim;
+    end case;
 end process;
 
-DP: datapath port map(DataIn,
-                      C,
-							 clk,
-							 reset,
-							 N,
-							 Z,
-							 cout,
-							 ov,
-							 DataOut,
-							 R0,
-							 R1,
-							 R2,
-							 R3,
-							 R4,
-							 R5,
-							 R6,
-							 R7);
+N    <= N_dp;
+Z    <= Z_dp;
+cout <= cout_dp;
+ov   <= ov_dp;
+
+DP: datapath
+port map(
+    DataIn  => DataIn,
+    C       => C,
+    clk     => clk,
+    reset   => reset,
+    N       => N_dp,
+    Z       => Z_dp,
+    cout    => cout_dp,
+    ov      => ov_dp,
+    DataOut => DataOut,
+    R0      => R0,
+    R1      => R1,
+    R2      => R2,
+    R4      => R4
+);
 
 end arq;
--- datapath.vhd
+++ datapath.vhd
@@ -3,11 +3,11 @@ USE IEEE.std_logic_1164.all;
 
 Entity datapath is
 Port(DataIn : in std_logic_vector(7 downto 0);
-     C : in std_logic_vector(27 downto 0);
-	  clk,reset : in std_logic;
-	  N,Z,cout,ov : out std_logic;
-	  DataOut,R0,R1,R2,R3,R4,R5,R6,R7 : out std_logic_vector(7 downto 0)
-	  );
+     C : in std_logic_vector(34 downto 0);
+     clk,reset : in std_logic;
+     N,Z,cout,ov : out std_logic;
+     DataOut,R0,R1,R2,R4 : out std_logic_vector(7 downto 0)
+     );
 end datapath;
 
 architecture arq of datapath is
@@ -15,87 +15,129 @@ architecture arq of datapath is
 component reg8 is
 Port(D : in std_logic_vector(7 downto 0);
      clk,reset,carga : in std_logic;
-	  q : out std_logic_vector(7 downto 0)
-	  );
+     q : out std_logic_vector(7 downto 0)
+     );
 end component;
 
 component mux4_1 is
 Port(A,B,C,D : in std_logic_vector(7 downto 0);
      sel : in std_logic_vector(1 downto 0);
-	  y : out std_logic_vector(7 downto 0)
-	  );
+     y : out std_logic_vector(7 downto 0)
+     );
 end component;
 
 component soma_sub is
 Port(A,B : in std_logic_vector(7 downto 0);
      op : in std_logic;
-	  N,Z,cout,ov : out std_logic;
-	  S : out std_logic_vector(7 downto 0)
-	  );
+     N,Z,cout,ov : out std_logic;
+     S : out std_logic_vector(7 downto 0)
+     );
 end component;
 
 component ULA is
 Port(A : in std_logic_vector(7 downto 0);
      op : in std_logic;
-	  S : out std_logic_vector(7 downto 0)
-	  );
+     S : out std_logic_vector(7 downto 0)
+     );
 end component;
 
 type bus_d is array (0 to 7) of std_logic_vector(7 downto 0);
 
 signal S_R,S_Q : bus_d;
- 
-signal DataUla,aux : std_logic_vector(7 downto 0);
-signal opA_mux,opB_mux,opC_mux : std_logic_vector(7 downto 0);
-signal OutSoma,OutShift : std_logic_vector(7 downto 0);
+signal DataUla1,aux : std_logic_vector(7 downto 0);
+signal opA1_mux,opB1_mux,opA2_mux,opB2_mux : std_logic_vector(7 downto 0);
+signal opC_mux : std_logic_vector(7 downto 0);
+signal OutSoma1,OutSoma2,OutShift : std_logic_vector(7 downto 0);
 
 begin
 
-G0: for i in 0 to 7 generate
-     M: S_R(i) <= DataIn when c(i) = '0' else DataUla;
-     R: reg8 port map(S_R(i),clk,reset,c(8+i),S_Q(i));
-    end generate;
+-- Somente os registradores usados pelo algoritmo sao instanciados.
+-- R0 guarda 1, R1 guarda o contador, R2 guarda o numero atual e R4 guarda a soma.
+S_R(0) <= DataIn when C(0) = '0' else DataUla1;
+REG0: reg8 port map(S_R(0),clk,reset,C(8),S_Q(0));
 
-M8A:with c(18 downto 16) select
-     opA_mux <= S_Q(0) when "000",
-	             S_Q(1) when "001",
-			       S_Q(2) when "010",
-			       S_Q(3) when "011",
-			       S_Q(4) when "100",
-			       S_Q(5) when "101",
-			       S_Q(6) when "110",
-			       S_Q(7) when others;
+-- R1 recebe a saida dedicada do segundo somador quando C(1) = 1.
+S_R(1) <= DataIn when C(1) = '0' else OutSoma2;
+REG1: reg8 port map(S_R(1),clk,reset,C(9),S_Q(1));
 
-		
-M8B:with c(21 downto 19) select
-     opB_mux <= S_Q(0) when "000",
-	             S_Q(1) when "001",
-			       S_Q(2) when "010",
-			       S_Q(3) when "011",
-			       S_Q(4) when "100",
-			       S_Q(5) when "101",
-			       S_Q(6) when "110",
-			       S_Q(7) when others;		
-		
-M4B: Mux4_1 port map(S_Q(4),S_Q(5),S_Q(6),S_Q(7),c(23 downto 22),opC_mux);
+S_R(2) <= DataIn when C(2) = '0' else DataUla1;
+REG2: reg8 port map(S_R(2),clk,reset,C(10),S_Q(2));
 
-M2: soma_sub port map(opA_mux,opB_mux,c(24),N,Z,Cout,ov,OutSoma);
+S_R(4) <= DataIn when C(4) = '0' else DataUla1;
+REG4: reg8 port map(S_R(4),clk,reset,C(12),S_Q(4));
 
-M3: ULA port map(opC_mux,c(25),OutShift);
+-- Posicoes sem registrador permanecem em zero, preservando os muxes originais.
+-- O zero usado pela FSM e tirado daqui, selecionando a posicao de R3.
+-- Nao existe um R3 fisico: S_Q(3) esta ligado direto em zero.
+S_Q(3) <= (others => '0');
+S_Q(5) <= (others => '0');
+S_Q(6) <= (others => '0');
+S_Q(7) <= (others => '0');
 
-aux <= OutSoma when c(26) = '0' else OutShift;
-DataUla <= aux when c(27) = '0' else opA_mux;
+M8A1: with C(18 downto 16) select
+    opA1_mux <= S_Q(0) when "000",
+                S_Q(1) when "001",
+                S_Q(2) when "010",
+                S_Q(3) when "011",
+                S_Q(4) when "100",
+                S_Q(5) when "101",
+                S_Q(6) when "110",
+                S_Q(7) when others;
+
+M8B1: with C(21 downto 19) select
+    opB1_mux <= S_Q(0) when "000",
+                S_Q(1) when "001",
+                S_Q(2) when "010",
+                S_Q(3) when "011",
+                S_Q(4) when "100",
+                S_Q(5) when "101",
+                S_Q(6) when "110",
+                S_Q(7) when others;
+
+M8A2: with C(30 downto 28) select
+    opA2_mux <= S_Q(0) when "000",
+                S_Q(1) when "001",
+                S_Q(2) when "010",
+                S_Q(3) when "011",
+                S_Q(4) when "100",
+                S_Q(5) when "101",
+                S_Q(6) when "110",
+                S_Q(7) when others;
+
+M8B2: with C(33 downto 31) select
+    opB2_mux <= S_Q(0) when "000",
+                S_Q(1) when "001",
+                S_Q(2) when "010",
+                S_Q(3) when "011",
+                S_Q(4) when "100",
+                S_Q(5) when "101",
+                S_Q(6) when "110",
+                S_Q(7) when others;
+
+M4B: mux4_1 port map(S_Q(4),S_Q(5),S_Q(6),S_Q(7),
+                     C(23 downto 22),opC_mux);
+
+-- Primeiro somador: caminho original da DataUla.
+-- Ele incrementa R2 e, no outro estado, acumula o valor em R4.
+SOMA1: soma_sub port map(opA1_mux,opB1_mux,C(24),
+                         open,open,open,open,OutSoma1);
+
+-- Segundo somador: caminho dedicado a R1 e flags do contador.
+-- Ele decrementa R1 e gera a flag Z usada para sair do loop.
+SOMA2: soma_sub port map(opA2_mux,opB2_mux,C(34),
+                         N,Z,cout,ov,OutSoma2);
+
+M3: ULA port map(opC_mux,C(25),OutShift);
+
+aux <= OutSoma1 when C(26) = '0' else OutShift;
+DataUla1 <= aux when C(27) = '0' else opA1_mux;
 
 DataOut <= opC_mux;
+-- DataOut seleciona R4, que e onde a soma final fica guardada.
 
-R0 <=S_Q(0);
-R1 <=S_Q(1);
-R2 <=S_Q(2);
-R3 <=S_Q(3);
-R4 <=S_Q(4);
-R5 <=S_Q(5);
-R6 <=S_Q(6);
-R7 <=S_Q(7);
+R0 <= S_Q(0);
+R1 <= S_Q(1);
+R2 <= S_Q(2);
+R4 <= S_Q(4);
 
 end arq;
-
